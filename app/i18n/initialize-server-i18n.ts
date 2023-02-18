6fce665a17870c34a4829ebfa2e1d54a89b01189
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2023-02-18 21:10:38 +0800

    0.3.4 (#34)
    * 0.3.4
    
    1. Fix i18n SSR
    2. Added language switcher component
    3. Updated packages

--- app/i18n/initialize-server-i18n.ts
+++ app/i18n/initialize-server-i18n.ts
@@ -10,17 +10,19 @@ async function initializeServerI18n(request: Request, context: EntryContext) {
   const instance = createInstance();
   const lng = await i18next.getLocale(request);
   const ns = [...i18n.defaultNS, ...i18next.getRouteNamespaces(context)];
-  const en = createLanguageBundle('en');
+  const bundle = createLanguageBundle(lng);
 
   await instance
     .use(initReactI18next)
     .use(Backend)
     .init({
       ...i18n,
+      supportedLngs: i18n.supportedLanguages,
+      fallbackLng: i18n.fallbackLanguage,
       lng,
       ns,
       resources: {
-        en,
+        [lng]: bundle,
       },
     });
 
